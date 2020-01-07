097a6aef266756594ae14efdd37b7931f0e15caf
Brandon Carter <bcarteratx@gmail.com>
2020-01-06 16:39:52 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,23 @@
 const cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-let cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else { alert("Sorry, try again");}
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+	  	console.log("You found a match!");
+		} else {
+	  		console.log("Sorry, try again.");
+		};
+	};
 };
 
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
 
+flipCard(0);
+flipCard(1);
 
-console.log("User flipped" + " " + cardTwo);
-console.log(cardsInPlay);
 
